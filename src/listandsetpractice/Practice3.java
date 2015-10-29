8616863af69ed1dad9a935d9b5d4723f3edb7fea
jbrown99 <jbrown99@L11708042771.wctc.msft>
2015-10-29 16:33:04 -0500

    added dog class to project and started working on practice3

---
+++ src/listandsetpractice/Practice3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listandsetpractice;
+
+/**
+ *
+ * @author jbrown99
+ */
+public class Practice3 {
+    
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Brown","Josh","111-11-1111");
+        Employee e2 = new Employee("Doe","Jane","456-02-2345");
+        
+        Dog d1 = new Dog("Ralphie",765);
+        Dog d2 = new Dog("Lucy",214);
+        
+        
+        
+    }
+    
+    
+}
